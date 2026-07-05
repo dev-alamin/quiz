a12ce373c6a09ad29acb0a5fca5e6a00abcc41d6
Al Amin <hmalaminmb4@gmail.com>
2026-07-05 19:23:21 +0600

    renderQuiz partial

--- quiz.js
+++ quiz.js
@@ -1,6 +1,8 @@
-let currentIndex = null;
-let result = [];
-let questions = [];
+"use strict";
+
+let currentIndex   = 0;
+let result         = [];
+let questions      = [];
 let selectedAnswer = null;
 
 //  Utility function for DOM selection 
@@ -9,18 +11,21 @@ function $(e) { return document.getElementById(e); }
 // ----------------------------------
 // DOM selection 
 // ----------------------------------
-const startScreen = $('screen-start');
+const startScreen    = $('screen-start');
 const questionScreen = $('screen-question');
-const resultScreen = $('screen-result');
-const errorScreen = $('screen-error');
+const resultScreen   = $('screen-result');
+const errorScreen    = $('screen-error');
+const optionList     = $( 'option-list' );
+const questionText   = $( 'question-text' );
 
 const btnStart = $('btn-start');
+const btnNext = $( 'btn-next' );
 
 const screen = {
-    'start': startScreen,
+    'start'   : startScreen,
     'question': questionScreen,
-    'result': resultScreen,
-    'error': errorScreen
+    'result'  : resultScreen,
+    'error'   : errorScreen
 };
 
 /**
@@ -39,22 +44,62 @@ function showScreen(s) {
     screen[s].classList.add('active');
 }
 
+/**
+ * Load actual data(questions) from api
+ * @returns void
+ */
 async function loadQuestions() {
     try {
-        const res = await fetch('data.json');
+        const res = await fetch('questions.json');
+
         if (!res.ok) {
             throw new Error(`HTTP Error: Status - ${res.status}`);
             return;
         }
 
-        data = await res.json();
-        questions = data;
+        const data = await res.json();
+        questions = data; // Inject questions
     } catch (err) {
         console.log(err);
+        showScreen( 'error' );
     }
 }
 
+function renderQuestion(){
+    const q        = questions[currentIndex];
+    const qNumber  = currentIndex + 1;
+    const totalQ   = questions.length;
+    const progress = ( qNumber / totalQ ) * 100;
+
+    $( 'progress-text' ).textContent = `Question ${qNumber} of total ${totalQ}`;
+    $( 'progress-bar' ).style.width = `${progress}%`;
+
+    console.log(progress);
+
+    $( 'question-text' ).textContent = q.question;
+
+    btnNext.textContent = currentIndex == totalQ ? 'See Result' : 'Next';
+}
+
+function handleNext(){
+    currentIndex++;
+
+    if( currentIndex < questions.length ) {
+        renderQuestion();
+        showScreen( 'question' );
+    }else{
+        showScreen( 'result' );
+    }
+
+}
+
+btnNext.addEventListener( 'click', handleNext );
+
 (async function() {
     await loadQuestions();
-    console.log(questions);
+
+    btnStart.addEventListener( 'click', function(){
+        showScreen( 'question' );
+        renderQuestion();
+    });
 })();
